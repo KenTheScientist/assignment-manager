d30fef2167ac74fff6e40570205e664050bc71e3
Kenneth Thomson <ken.d.thomson@gmail.com>
2019-07-31 21:09:12 -0400

    test

--- index.php
+++ index.php
@@ -59,15 +59,14 @@ Author: Ken
 				Your Tasks:
 				<br>
 				<br>
-				Input Task: 
-				<form id="myForm" action="action_page.php" method="post">
-					
+				 
+				Input Task:<form id="myForm" action="action_page.php" method="post">
 					<input type="text" placeholder="NAME" name="r_name" required />
 					<input type="text" placeholder="MONTH" name="r_m" required />
 					<input type="text" placeholder="DAY" name="r_d" required />
 					<input type="text" placeholder="YEAR" name="r_y" required />
 					<input type="text" placeholder="CLASS" name="r_class" required />
-					<button type="submit" class="submit_btn" id = "in_main">+</button><br>
+					<button type="submit" class="submit_btn" id = "plus_sign">+</button><br>
 				</form>
 				<!--
 			This is where we put tasks, input tasks, etc.
