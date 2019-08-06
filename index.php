9c12ce2a71118e31b6c6bd8ff1b8a0d7c8c25a80
Kenneth Thomson <ken.d.thomson@gmail.com>
2019-08-06 08:48:05 -0400

    test

--- index.php
+++ index.php
@@ -77,42 +77,13 @@ Author: Ken
 				$student = $_SESSION['student'];
 				$query = "SELECT * FROM task INNER JOIN assignment ON assignment.task = task.task AND assignment.student = $student";
 				$statement = $db->prepare($query);
-                $statement->execute();   
-                $assignment_data = $statement->fetch(PDO::FETCH_BOTH);
+                $statement->execute();  
 				
-				$query = "SELECT * FROM task INNER JOIN assignment ON assignment.task = task.task AND assignment.student = $student";
-
-				$result = pg_query($query);
-
-				$i = 0;
-				echo '<table><tr>';
-				while ($i < pg_num_fields($result))
-				{
-					$fieldName = pg_field_name($result, $i);
-					echo '<td>' . $fieldName . '</td>';
-					$i = $i + 1;
+				for($i = 1; $i < 1000; $i++){
+					$assignment_data = $statement->fetch(PDO::FETCH_ASSOC);
+					echo "<br>".$assignment_data["task_name"];
 				}
-				echo '</tr>';
-				$i = 0;
-
-				while ($row = pg_fetch_row($result)) 
-				{
-					echo '<tr>';
-					$count = count($row);
-					$y = 0;
-					while ($y < $count)
-					{
-						$c_row = current($row);
-						echo '<td>' . $c_row . '</td>';
-						next($row);
-						$y = $y + 1;
-					}
-					echo '</tr>';
-					$i = $i + 1;
-				}
-				pg_free_result($result);
-
-				echo '</table>';
+				
 				
 				?>
 				
