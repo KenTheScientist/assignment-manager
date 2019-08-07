eccc16c3ba6f44e7d504204a4515c28ebdd9179a
Kenneth Thomson <ken.d.thomson@gmail.com>
2019-08-07 09:14:05 -0400

    test

--- modules/head.php
+++ modules/head.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" type="text/css" href="<?php echo $ROOT; ?>styles/normalize.css" />
 
 <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $ROOT; ?>styles/kens_version.css" />
-
+<link rel="icon" href="/favicon.ico"/>
 <meta charset="UTF-8">
 <meta name="author" content="Kenneth Thomson">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
